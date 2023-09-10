e5a70598124823875fbc1e2243ac8794ea18ef26
Colin Devick <colindevick.cd@gmail.com>
2023-09-09 19:08:20 -0500

    ModItem Class added

---
+++ forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/ModItems.java
@@ -0,0 +1,5 @@
+package com.devickcolin.speedboat;
+
+public class ModItems extends SpeedBoat {
+
+}
--- forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/SpeedBoat.java
+++ forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/SpeedBoat.java
@@ -37,8 +37,6 @@ public class SpeedBoat
     public static final String MOD_ID = "speedboat";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
@@ -51,6 +49,8 @@ public class SpeedBoat
 
     // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
     public static final RegistryObject<Item> SPEED_BOAT = ITEMS.register("speed_boat", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<CreatibeModeTab> 
+    
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
    /* public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.COMBAT)
@@ -61,46 +61,16 @@ public class SpeedBoat
 
     public SpeedBoat()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-      //BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-      //  CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(SPEED_BOAT);
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
