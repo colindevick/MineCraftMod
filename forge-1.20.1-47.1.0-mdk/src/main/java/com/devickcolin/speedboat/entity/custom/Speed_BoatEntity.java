b462ea6f1d9a75ae63ffbca29993abf7c24c08b4
Colin Devick <colindevick.cd@gmail.com>
2023-09-18 19:48:19 -0500

    Fixed bug of OakBoat spawning instead of the SpeedBoatEntity

--- forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/entity/custom/Speed_BoatEntity.java
+++ forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/entity/custom/Speed_BoatEntity.java
@@ -1,15 +1,9 @@
 package com.devickcolin.speedboat.entity.custom;
 
 
-import net.minecraft.util.ByIdMap;
-import java.util.function.IntFunction;
-import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.registries.RegistryObject;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegistrar;
@@ -24,7 +18,7 @@ public class Speed_BoatEntity extends Boat implements GeoEntity{
 	   }
 
 	  public Speed_BoatEntity(Level p_38293_, double p_38294_, double p_38295_, double p_38296_) {
-	      this(EntityType.BOAT, p_38293_);
+	      this(ModEntityCreator.SPEED_BOATS.get(), p_38293_);
 	      this.setPos(p_38294_, p_38295_, p_38296_);
 	      this.xo = p_38294_;
 	      this.yo = p_38295_;
@@ -36,7 +30,7 @@ public class Speed_BoatEntity extends Boat implements GeoEntity{
 	
 	public AnimatableInstanceCache getAnimatableInstanceCache() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.cache;
 	}
 
 	@Override
