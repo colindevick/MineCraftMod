3f1abb777328aa1586e7878f19ef3b6f991e8ee4
Colin Devick <colindevick.cd@gmail.com>
2023-10-01 21:11:43 -0500

    Added effects to ethanol so it poisons on drinking

--- forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/entity/custom/items/ModPotions.java
+++ forge-1.20.1-47.1.0-mdk/src/main/java/com/devickcolin/speedboat/entity/custom/items/ModPotions.java
@@ -1,8 +1,10 @@
 package com.devickcolin.speedboat.entity.custom.items;
 
-import com.devickcolin.speedboat.entity.custom.ModHandler;
+import com.devickcolin.speedboat.entity.custom.core.ModHandler;
 
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,7 +19,7 @@ import net.minecraftforge.registries.RegistryObject;
 				DeferredRegister.create(ForgeRegistries.POTIONS, ModHandler.MOD_ID);
 		
 		public final static RegistryObject<Potion> ETHONAL = POTIONS.register("ethonal",
-				() -> new Potion());
+				() -> new Potion("Ethonal", new MobEffectInstance(MobEffects.POISON, 9600)));
 		
 		public static void register(IEventBus eventBus) {
 			POTIONS.register(eventBus);
